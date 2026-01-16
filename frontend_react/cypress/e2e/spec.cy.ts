e05fb61b843010721acb6b8956a7b679ae6f613e
Michel Mahadeva <michel.mahadeva@edu.tbz.ch>
2026-01-16 16:23:03 +0100

    change admin to user

--- frontend_react/cypress/e2e/spec.cy.ts
+++ frontend_react/cypress/e2e/spec.cy.ts
@@ -7,7 +7,7 @@ describe("home page spec", () => {
 describe("check login", () => {
   it("passes", () => {
     cy.visit("http://localhost:3000/login");
-    cy.get('input[name="email"]').type("admin@example.com");
+    cy.get('input[name="email"]').type("user@example.com");
     cy.get('input[name="password"]').type("1234");
     cy.get('button[name="submit"]').click();
   });
